29df9436ecef6552184c6847a66e6483801ecd0b
Daniela <d.marino10@uniandes.edu.co>
2020-05-21 19:49:44 -0700

    bird flies!

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float flyForce=200f;
+    void Awake(){
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(0,flyForce));
+        }
+    }
+}
