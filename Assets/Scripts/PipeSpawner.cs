6e044266cca9d9be07c51b1714cdf5d20681df50
Daniela <d.marino10@uniandes.edu.co>
2020-05-21 20:27:34 -0700

    now we have pipes

---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject pipePrefab;
+
+    public float timeBetweenPipes = 0.5f;
+
+    public float maxHeight=2;
+    public float minHeight=-2;
+    
+    public void Start()
+    {
+         InvokeRepeating ("SpawnPipes", 1.0f, timeBetweenPipes);
+    }
+
+
+    void SpawnPipes(){
+
+        float y = UnityEngine.Random.Range (minHeight, maxHeight);
+        float x = gameObject.transform.position.x;
+
+        Instantiate (pipePrefab, new Vector3(x,y,0), Quaternion.identity);
+
+    }
+
+    
+}
---
+++ Assets/Scripts/pipeMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeMovement : MonoBehaviour
+{
+
+    public float movement=2;
+    void Update()
+    {
+
+        Vector3 newPos = gameObject.transform.position;
+
+        newPos.x = newPos.x - movement*Time.deltaTime;
+
+        gameObject.transform.position = newPos;
+        
+    }
+    
+}
